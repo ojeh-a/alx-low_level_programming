0bd47ad6187bc2ac1f156bd59c225a5c71d19a7c
genie <ojehdayo@gmail.com>
2022-10-15 18:16:11 +0100

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet 
+ *
+ * Return: 0(end)
+ */
+int main(void)
+{
+	char letter_a = 'a';
+	char letter_z = 'z';
+	char ALPHA_A = 'A';
+	char ALPHA_Z = 'Z';
+
+	for (letter_a <= letter_z; letter_a++)
+	{
+		putchar(letter_a);
+	}
+
+	for (ALPHA_A <= ALPHA_Z; ALPHA_A++)
+	{
+		putchar(ALPHA_A);
+	}
+	putchar('\n');
+	return (0)
+}
