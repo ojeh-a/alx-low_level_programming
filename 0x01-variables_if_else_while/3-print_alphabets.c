132d97b9ab08ef48294419f20b148cd064f05784
genie <ojehdayo@gmail.com>
2022-10-15 18:43:08 +0100

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ int main(void)
 	char ALPHA_A = 'A';
 	char ALPHA_Z = 'Z';
 
-	while (; letter_a <= letter_z; letter_a++)
+	for (; letter_a <= letter_z; letter_a++)
 	{
 		putchar(letter_a);
 	}
 
-	while (; ALPHA_A <= ALPHA_Z; ALPHA_A++)
+	for (; ALPHA_A <= ALPHA_Z; ALPHA_A++)
 	{
 		putchar(ALPHA_A);
 	}
