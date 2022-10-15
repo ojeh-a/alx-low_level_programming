6097bc6e6536528db7f7a325217fafacb09e4a39
genie <ojehdayo@gmail.com>
2022-10-15 19:00:10 +0100

    alphabet q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+	char alphaz = 'z';
+	char q = 'q';
+	char e = 'e';
+
+	for ( alpha <= alphaz && != e, q; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0)
+}
