690fac8ffaad4736593c3030bb84dd9136b70bde
genie <ojehdayo@gmail.com>
2022-10-16 02:56:43 +0100

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print a single number
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	unsigned char;
+
+	while (unsigned char <= '0';)
+		printf("%c\n");
+	return (0);
+}
