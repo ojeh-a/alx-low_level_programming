c8b9624fb7dc1b028bd076ba4f622231681f6f5e
genie <ojehdayo@gmail.com>
2022-10-17 11:12:38 +0100

    piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print piece of art
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n)
+	return (0);
+}
