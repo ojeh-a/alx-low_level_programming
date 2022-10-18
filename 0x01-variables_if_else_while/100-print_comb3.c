77456a090c0a02166d8cfa3658cde97cde0be1dd
genie <ojehdayo@gmail.com>
2022-10-18 05:20:14 +0100

    ggg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int a;
 	int b;
-	a = '0';
-	b = '1';
+	a = 0;
+	b = 1;
 
-	while (a <= '8')
+	while (a <= 8)
 	{
-		while (b <= '9')
+		while (b <= 9)
 		{
 			while (a != b)
 			{
 				putchar(48 + a);
 				putchar(48 + b);
-				if (a <= '8')
+				if (a <= 8)
 				{
 				putchar(',');
 				putchar(' ');
