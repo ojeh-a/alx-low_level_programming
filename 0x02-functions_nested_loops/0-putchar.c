9f30e85c62ae35fca4d63d23b364acd4d6ce22c5
genie <ojehdayo@gmail.com>
2022-10-19 11:28:21 +0100

    u

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,6 +12,15 @@ int main(void)
 {
 	int;
 
-	_putchar("_putchar\n", int);
-	return 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0)
 }
