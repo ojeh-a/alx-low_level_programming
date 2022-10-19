e89d845d9c6c1dfd5c95a599bae2962e69b9e1bc
genie <ojehdayo@gmail.com>
2022-10-19 14:48:44 +0100

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+  * print_sign - checks for alphabetic character
+  *
+  *  @n: character to be checked
+  *
+  * Return: always 0, 1 or -1
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+		return (-1);
+}
