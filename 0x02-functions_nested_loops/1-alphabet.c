ecdaabd53944eeae97f25116423c9652a8ae3f5c
genie <ojehdayo@gmail.com>
2022-10-19 12:01:27 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	/**
+	  *prints alphabets in lowercase
+	  *
+	  * Return: 0
+	  */
+	void print_alphabet(void)
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+			_putchar('\n');
+	}
+	return (0);
+}
