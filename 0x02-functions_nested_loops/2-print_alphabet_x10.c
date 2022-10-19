b42edff221f443c72efe5ee66dff1fe060165cfd
genie <ojehdayo@gmail.com>
2022-10-19 12:51:06 +0100

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - print 10 times the alphabet
+  *
+  *Return: Always 0
+  */
+void print_alphabet_x10(void)
+{
+	int a, b
+		for (a = 0; <= 9; a++)
+		{
+			for (b = 'a'; b <= 'z' b++)
+			{
+				_putchar(b);
+			}
+			_putchar('\n');
+		}
+}
