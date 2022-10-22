24b605bf6d521e3de05d0176808d2c44288dfced
genie <ojehdayo@gmail.com>
2022-10-22 17:30:43 +0530

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (m = 0; m <= 10; m++)
+	{
+		for (n = 0; n<= 14; n++)
+		{
+			_putchar('0' + n);
+		}
+		_putchar('\n');
+	}
+}
