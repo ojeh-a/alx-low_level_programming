852e359517c5344ce4ab8493209c98b6580eadf7
genie <ojehdayo@gmail.com>
2022-10-25 23:41:10 +0100

    task completed

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints a string to stdout
@@ -13,5 +14,5 @@ void _puts(char *str)
 	{
 		putchar(*str++);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
