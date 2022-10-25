5e38c17990069733a32f0266ba4aff88a47a020d
genie <ojehdayo@gmail.com>
2022-10-25 22:29:54 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+
+	while ([a] != '\0')
+	a++;
+	{
+		for (i = a - 1; a >= 0; a--)
+		_putchar(s[a]);
+	}
+	putchar ('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string
+ *
+ * Return: reverse string
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for(i = 0; i < c; i++)
+	{
+		c--;
+		rev = s[i];
+		s[i] =s [c];
+		s[c] = rev;
+	}
+}
