514e66f69cadefca5bb42fd853e0674d87f19821
genie <ojehdayo@gmail.com>
2022-10-30 09:26:46 +0100

    Betty satisfied

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,35 +9,36 @@ int main(void)
 {
 	int a, b, c, d, x, y;
 
-		for (a = '0'; a <= '9'; a++)
-		{
-			for (b = '0'; b <= '9'; b++)
-			{
-				for (c = '0'; c <= '9'; c++)
-				{
-					for (d = '0'; d <= '9'; d++)
-					{
-						x = (a * 10) + b;
-						y = (c * 10) + d;
 
-						if (x < y)
-						{
-							putchar(a);
-							putchar(b);
-							putchar(' ');
-							putchar(c);
-							putchar(d);
-							if (a == 57 && b == 56 && c == 57 && d == 57)
-							{
-								break;
-							}
-							putchar(',');
-							putchar(' ');
-						}
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					x = (a * 10) + b;
+					y = (c * 10) + d;
+					if (x < y)
+					{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+
+					if (a == 57 && b == 56 && c == 57 && d == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
 		}
-			putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
