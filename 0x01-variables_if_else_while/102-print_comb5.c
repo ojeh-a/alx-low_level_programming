fa6f95821848ba07f22b02ce7907bc7e4d576bed
genie <ojehdayo@gmail.com>
2022-10-30 08:42:02 +0100

    god abeg

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+  *main - prints all posible cmbinations of two numbers
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int a, b, c, d;
+
+		for (a = '0'; a <= '9'; a++)
+		{
+			for (b = '0'; b <= '9'; b++)
+			{
+				for (c = 0; c <= '9'; c++)
+				{
+					for (d = '1'; d <= '9'; d++)
+					{
+						if (a + b < c + d)
+						{
+							putchar(a);
+							putchar(b);
+							putchar(' ');
+							putchar(c);
+							putchar(d);
+							if (a == 57 && b == 56 && c == 57 && d == 57)
+							{
+								break;
+							}
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+			putchar('\n');
+	return (0);
+}
