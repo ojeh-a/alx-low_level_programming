5dd0989a0cfdd56ed3f26e85f4eedea185fab125
genie <ojehdayo@gmail.com>
2022-11-01 16:54:39 +0100

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,12 +13,14 @@ void print_square(int size)
 	int a;
 	int b;
 
+	if (size <= 0)
+		_putchar('\n');
 
 	for (a = 0; a < size; a++)
 	{
 		for (b = 0; b < size; b++)
 		{
-			_putchar('#');
+				putchar('#');
 		}
 		putchar('\n');
 	}
