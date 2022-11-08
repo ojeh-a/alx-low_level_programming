3f902e31a6a737a1c377567201d64abfceeac203
genie <ojehdayo@gmail.com>
2022-11-08 06:27:18 +0100

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts2
@@ -16,11 +17,11 @@ void puts2(char *str)
 	{
 		if((i % 2) == 0)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
 		else
 			continue;
 	}
-	_putchar('\n');
+	putchar('\n');
 
 }
