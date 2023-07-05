b12426be7b946f733962722d3d1403205219c887
david joy <davidjoy.win@gmail.com>
2023-07-05 23:24:08 +0100

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - entry point.
+  *
+  *@argc: argument count.
+  *@argv: argument vector.
+  *Return: nothing.
+  */
+
+int main(int argc, char *argv[])
+{
+	int count = (argc - 1);
+
+	argv--;
+	if (argc > 1)
+
+	{
+		printf("%d\n", count);
+	}
+	return (0);
+}
+
