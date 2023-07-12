5933bd08bf54bc59d9204ffdde4beae80e193ecd
Ojeh Agbaje <ojehdayo@gmail.com>
2023-07-12 10:32:13 +0100

    requirement error

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,7 +8,7 @@
   */
 int _atoi(char *s)
 {
-	int result = 0;
+	int reslt = 0;
 	int sign = 1;
 	int i = 0;
 
@@ -27,8 +27,8 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		reslt = reslt * 10 + (s[i] - '0');
 	}
-	return (result *  sign);
+	return (reslt *  sign);
 }
 
