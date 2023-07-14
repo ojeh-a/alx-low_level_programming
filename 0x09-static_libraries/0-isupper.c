f6f23d7d50153c436e0e05c3b0715265096c7cd6
Ojeh Agbaje <ojehdayo@gmail.com>
2023-07-14 11:00:38 +0100

    0-isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,9 @@
 #include "main.h"
 /**
-  *_isupper - checks for uppercase.
-  *@c: character to be checked
-  *Return: 0 or 1
+  *_isupper - determines an uppercase.
+  *@c: character
+  *Return: int
   */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }
--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-c_files=$(find . -name "*.c")
-for c_file in $c_files; do
-gcc -c $c_file
-done
-object_files=$(find . -name "*.o")
-ar -rcs liball.a $object_files 
-
