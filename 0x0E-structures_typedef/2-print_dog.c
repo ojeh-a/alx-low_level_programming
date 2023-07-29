ae02a43f0016ce97bb2649800e1934be34e2cebd
Ojeh Agbaje <ojehdayo@gmail.com>
2023-07-29 10:18:48 +0100

    nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		} else
 		{
-			printf("Name: nil\n");
+			printf("Name: (nil)\n");
 		}
 		printf("Age: %1f\n", d->age);
 		if (d->owner != NULL)
@@ -28,7 +28,7 @@ void print_dog(struct dog *d)
 			printf("Owner: %s\n", d->owner);
 		} else
 		{
-			printf("Owner: nil\n");
+			printf("Owner: (nil)\n");
 		}
 	}
 }
