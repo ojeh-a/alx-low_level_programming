b51eb94dd52e50da583be704832f2c2facce17d2
Ojeh Agbaje <ojehdayo@gmail.com>
2023-08-17 22:53:52 +0100

    betty requirements

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * print_strings - prints strings.
+ * @separator: strings to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * Return: nothing.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
@@ -10,13 +17,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(args, char *);
+
 		if (str != NULL)
 		{
 			printf("%s", str);
 		}
 		else
 		{
-			printf ("(nil)");
+			printf("(nil)");
 		}
 		if (i < n - 1 && separator != NULL)
 		{
@@ -26,4 +34,3 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_end(args);
 	printf("\n");
 }
-
