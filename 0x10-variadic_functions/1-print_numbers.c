1c11ecb7393bf78f46b725012ae57b054be59b5c
Ojeh Agbaje <ojehdayo@gmail.com>
2023-08-17 22:21:49 +0100

    isdk

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,18 +9,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list arguments;
 
-	va_start(args, n);
+	va_start(arguments, n);
 	for (unsigned int i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		printf("%d", va_arg(arguments, int));
 		if (i < n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
 	}
-	va_end(args);
+	va_end(arguments);
 	printf("\n");
 }
 
