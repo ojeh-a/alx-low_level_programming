5945317d88019d516412cad39ea0e061e9d558d4
Ojeh Agbaje <ojehdayo@gmail.com>
2023-08-17 16:47:04 +0100

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - prints opcodes
+ *
+ * Return: nothing.
+ */
+void print_opcodes(void)
+{
+}
+
+/**
+ * main - entry point, main function.
+ * @argc: argument count.
+ * @argv: argument vector
+ *
+ * Return: int.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *ptr = (unsigned char *)&print_opcodes;
+	int i;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", ptr[i]);
+		if (i < num_bytes - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
