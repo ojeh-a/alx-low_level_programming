d18a4ae831d80c9c0b11fc4f245f9287bf829eb8
Ojeh <ojehdayo@gmail.com>
2023-09-05 11:04:31 +0100

    long line

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filenme, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void exit_with_error(int exit_code, const char *message, const char *arg1, const char *arg2);
+void exit_with_error(int exit_code, const char *message,
+		const char *arg1, const char *arg2);
 
 #endif
