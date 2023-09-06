c07e8f044231eca982c726dead488174261ad43b
Ojeh <ojehdayo@gmail.com>
2023-09-06 11:01:02 +0100

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,69 +1,106 @@
 #include "main.h"
+#include <fcntl.h>
 
 /**
- * exit_with_error - exit with error if process fails
- * @exit_code: exit code
- * @message: error message
- * @arg: argument
- */
-void exit_with_error(int exit_code, const char *message, const char *arg1, const char *arg2)
-{
-    dprintf(STDERR_FILENO, message, arg1, arg2);
-    exit(exit_code);
-}
-
-/**
- * main - entry point
+ * main - main function (copies file content).
  * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success
+ * @argv: argument vector.
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
 	const char *file_from, *file_to;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
-		exit_with_error(97, "Usage: cp file_from file_to\n", argv[0], "");
+		exit_with_error(97, "Usage: cp file_from file_to\n", argv[0], -1);
 	}
 
-    	file_from = argv[1];
+	file_from = argv[1];
 	file_to = argv[2];
+	fd_from = open_source_file(file_from);
+	fd_to = open_destination_file(file_to);
+	copy_file_data(fd_from, fd_to);
 
-    	fd_from = open(file_from, O_RDONLY);
-    	if (fd_from == -1)
+	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		exit_with_error(98, "Error: Can't read from file %s\n", file_from, "");
-    	}
+		exit_with_error(100, "Error: Can't close fd\n", "", -1);
+	}
+	return (0);
+}
 
-    	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    	if (fd_to == -1)
+/**
+ * exit_with_error - exits the program with standard error if copy fails.
+ * @exit_code: exit code(97, 98, 99 or 100).
+ * @message: Error message.
+ * @arg1: first argument (file to copy from)
+ * @arg2: second argument(copy to).
+ */
+void exit_with_error(int exit_code, const char *message,
+		const char *arg1, int arg2)
+{
+	dprintf(STDERR_FILENO, message, arg1, arg2);
+	exit(exit_code);
+}
+
+/**
+ * open_source_file - opens source file.
+ * @file_from: filename
+ *
+ * Return: file descriptor
+ */
+int open_source_file(const char *file_from)
+{
+	int fd_from = open(file_from, O_RDONLY);
+
+	if (fd_from == -1)
 	{
-        	exit_with_error(99, "Error: Can't write to file %s\n", file_to, "");
-    	}
+		exit_with_error(98, "Error: Can't read from file %s\n", file_from, -1);
+	}
+	return (fd_from);
+}
 
-    	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    	{
-        	bytes_written = write(fd_to, buffer, bytes_read);
-        	if (bytes_written != bytes_read)
+/**
+ * open_destination_file - opens destination file
+ * @file_to: destination file.
+ *
+ * Return: file descriptor
+ */
+int open_destination_file(const char *file_to)
+{
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+
+	if (fd_to == -1)
+	{
+		exit_with_error(99, "Error: Can't write to file %s\n", file_to, -1);
+	}
+	return (fd_to);
+}
+
+/**
+ * copy_file_data - copies data from first argument to second argument.
+ * @fd_from: copy from here
+ * @fd_to: paste data here.
+ */
+void copy_file_data(int fd_from, int fd_to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
-            	exit_with_error(99, "Error: Can't write to file %s\n", file_to, "");
-        	}
-    	}
+			exit_with_error(99, "Error: Can't write to file %d\n", "", fd_to);
+		}
+	}
 
-    	if (bytes_read == -1)
-    	{
-        	exit_with_error(98, "Error: Can't read from file %s\n", file_from, "");
-    	}
-
-    	if (close(fd_from) == -1 || close(fd_to) == -1)
-    	{
-        	exit_with_error(100, "Error: Can't close fd\n", "", "");
-    	}
-
-    	return (0);
+	if (bytes_read == -1)
+	{
+		exit_with_error(98, "Error: Can't read from file %d\n", "", fd_from);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,6 +16,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filenme, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void exit_with_error(int exit_code, const char *message,
-		const char *arg1, const char *arg2);
+		const char *arg1, int arg2);
+int open_source_file(const char *file_from);
+int open_destination_file(const char *file_to);
+void copy_file_data(int fd_from, int fd_to);
 
 #endif
