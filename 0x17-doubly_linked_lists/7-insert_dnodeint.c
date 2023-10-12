f9804fe3c8c2cfda05a99103dcce8184fb9e886a
Ojeh <ojehdayo@gmail.com>
2023-10-12 22:41:22 +0100

    Doubly linked list, insert and delete at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_dnodint_at_index - Inserts a new node at a given position.
+ * @h: pointer to head
+ * @idx: index of the list where the new node is t0 be added
+ * @n: data for the new data.
+ *
+ * Return: Address of the new node or NULL if it fails.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h, *new;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	for (; idx != 1; idx--)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
+	temp->next = new;
+	return (new);
+}
