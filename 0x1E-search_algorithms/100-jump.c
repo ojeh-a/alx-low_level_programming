fbda9369eb85c919b1cb3bb57953e23c5dfd6800
Ojeh <ojehdayo@gmail.com>
2024-02-09 20:05:55 +0100

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value)
 	step = sqrt(size);
 	for (index = jump = 0; jump < size && array[jump] < value;)
 	{
-		printf("Value checked array[%ld] and [%d]\n", jump, array[jump]);
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
 		index = jump;
 		jump += step;
 	}
