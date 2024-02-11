669d2a789ff28dc74547d9394de989b1f606d9f8
Ojeh <ojehdayo@gmail.com>
2024-02-11 15:52:23 +0100

    Jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -34,7 +34,7 @@ listint_t * jump_list(listint_t *list, size_t size, int value)
 			node->index, jump->index);
 
 	for (; node->index < jump->index && node->n < value; node = node->next)
-		printf("value checked at index[%ld] = [%d]\n", node->index, node->n);
+		printf("Value checked at index[%ld] = [%d]\n", node->index, node->n);
 	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
 	return (node->n == value ? node : NULL);
